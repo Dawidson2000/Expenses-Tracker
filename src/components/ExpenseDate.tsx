ff4482f7df823a31d753c4919eab69ee0b2cd75c
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-10-18 13:43:44 +0200

    Added ExpenseDate component

---
+++ src/components/ExpenseDate.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FC } from 'react';
+
+export interface IExpenseDate {
+    date: Date,
+}
+
+const ExpenseDateWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 5.5rem;
+    height: 5.5rem;
+    border: 1px solid #ececec;
+    background-color: #2a2a2a;
+    color: white;
+    border-radius: 12px;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ExpenseDate_Month = styled.div`
+    font-size: 0.75rem;
+    font-weight: bold;
+    padding: 2px 0;
+`;
+
+const ExpenseDate_Year = styled.div`
+    font-size: 0.75rem;
+    padding: 2px 0;
+`;
+
+const ExpenseDate_Day = styled.div`
+   font-size: 1.5rem;
+   font-weight: bold;
+   padding: 2px 0;
+`;
+
+export const ExpenseDate: FC<IExpenseDate> = (props) => {
+
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+    return (
+        <ExpenseDateWrapper>
+            <ExpenseDate_Month>{month}</ExpenseDate_Month>
+            <ExpenseDate_Day>{day}</ExpenseDate_Day>
+            <ExpenseDate_Year>{year}</ExpenseDate_Year>
+        </ExpenseDateWrapper>
+
+    )
+}
